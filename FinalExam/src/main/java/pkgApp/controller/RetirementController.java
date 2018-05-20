fc8b945fd1cac6e6d1f9c545b84f5ac99f68d44d
Biggkahuna98 <sederquest.alex@gmail.com>
2018-05-19 22:18:13 -0400

    Done

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -65,8 +65,12 @@ public class RetirementController implements Initializable {
 		// "\\d*?" - means any decimal number
 		// "\\d*(\\.\\d*)?" means any decimal, then optionally a period (.), then
 		// decmial
-		hmTextFieldRegEx.put(txtYearsToWork, "\\d*?");
-		hmTextFieldRegEx.put(txtAnnualReturnWorking, "\\d*(\\.\\d*)?");
+		hmTextFieldRegEx.put(txtYearsToWork, "([0-9]|[1-3][0-9]|40)");
+		hmTextFieldRegEx.put(txtAnnualReturnWorking, "(0|0\\.[0-1]|0\\.[0][0-9]|\\.[0-1]|\\.[0][0-9])");
+		hmTextFieldRegEx.put(txtYearsRetired, "([0-9]|[1][0-9]|20)");
+		hmTextFieldRegEx.put(txtAnnualReturnRetired, "(0|0\\.[0-1]|0\\.[0][0-9]|\\.[0-1]|\\.[0][0-9])");
+		hmTextFieldRegEx.put(txtRequiredIncome, "(264[2-9]|26[5-9][0-9]|2[7-9][0-9]{2}|[3-9][0-9]{3}|10000)");
+		hmTextFieldRegEx.put(txtMonthlySSI, "([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|1[0-9]{3}|2[0-5][0-9]{2}|26[0-3][0-9]|264[0-2])");
 
 		// Check out these pages (how to validate controls):
 		// https://stackoverflow.com/questions/30935279/javafx-input-validation-textfield
@@ -97,26 +101,31 @@ public class RetirementController implements Initializable {
 		}
 
 		//
-		// TODO: Validate Working Annual Return %, accept only numbers and decimals
-		// TODO: Validate Years retired, accepted only decimals
-		// TODO: Validate Retired Annual Return %, accept only numbers and deciamls
-		// TODO: Validate Required Income, accept only decimals
-		// TODO: Validate Monthly SSI, accept only decimals
+		// DONE: Validate Working Annual Return %, accept only numbers and decimals
+		// DONE: Validate Years retired, accepted only decimals
+		// DONE: Validate Retired Annual Return %, accept only numbers and deciamls
+		// DONE: Validate Required Income, accept only decimals
+		// 	
 	}
 
 	@FXML
 	public void btnClear(ActionEvent event) {
 		System.out.println("Clear pressed");
 
-		// disable read-only controls
-		txtSaveEachMonth.setDisable(true);
-		txtWhatYouNeedToSave.setDisable(true);
+		txtSaveEachMonth.clear();
+		txtWhatYouNeedToSave.clear();
+		
+		Iterator it = hmTextFieldRegEx.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			TextField txtField = (TextField) pair.getKey();
+			
+			txtField.clear();
+			txtField.setDisable(false);
+		}
 
-		// Clear, enable txtYearsToWork
-		txtYearsToWork.clear();
-		txtYearsToWork.setDisable(false);
 
-		// TODO: Clear, enable the rest of the input controls. Hint! You already have a
+		// DONE: Clear, enable the rest of the input controls. Hint! You already have a
 		// HashMap of all the input controls....!!!!
 	}
 
@@ -128,8 +137,16 @@ public class RetirementController implements Initializable {
 		txtSaveEachMonth.setDisable(false);
 		txtWhatYouNeedToSave.setDisable(false);
 
-		// TODO: Calculate txtWhatYouNeedToSave value...
-		// TODO: Then calculate txtSaveEachMonth, using amount from txtWhatYouNeedToSave
+		// DONE: Calculate txtWhatYouNeedToSave value...
+		// DONE: Then calculate txtSaveEachMonth, using amount from txtWhatYouNeedToSave
 		// as input
+		Retirement ret = new Retirement(Integer.parseInt(txtYearsToWork.getText()), Double.parseDouble(txtAnnualReturnWorking.getText()), Integer.parseInt(txtYearsRetired.getText()), 
+				Double.parseDouble(txtAnnualReturnRetired.getText()), Double.parseDouble(txtRequiredIncome.getText()), Double.parseDouble(txtMonthlySSI.getText()));
+		
+		txtWhatYouNeedToSave.setText(Double.toString(ret.TotalAmountToSave()));
+		txtSaveEachMonth.setText(Double.toString(ret.MonthlySavings()));
+		
+		txtSaveEachMonth.setDisable(true);
+		txtWhatYouNeedToSave.setDisable(true);
 	}
 }
